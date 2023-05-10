1bcf29fd50c3efe54d2254e8942de847948f8f15
Grim8020 <shanegrim87@gmail.com>
2023-05-10 09:36:48 -0400

    store UI update

--- lib/merchandise_card_size_color.dart
+++ lib/merchandise_card_size_color.dart
@@ -1,5 +1,140 @@
 import 'package:flutter/material.dart';
 
+class MerchCard extends StatefulWidget {
+  const MerchCard({Key? key}) : super(key: key);
+
+  @override
+  State<MerchCard> createState() => _MerchCardState();
+}
+
+class _MerchCardState extends State<MerchCard> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.transparent,
+              child: Image.asset(
+                'images/DiscGoShopCart.png',
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 30,
+              child: Text('Dress like the DeGen Pros'),
+              color: Colors.grey,
+              alignment: Alignment.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 1.0,
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.6,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10),
+                  children: [
+                    CustomMerchCard(
+                      merchImage: 'images/male_small.png',
+                      merchPrice: '25.00',
+                      merchName: 'Graphic Tee',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomMerchCard extends StatefulWidget {
+  const CustomMerchCard(
+      {Key? key,
+      required this.merchImage,
+      required this.merchPrice,
+      required this.merchName})
+      : super(key: key);
+
+  final String merchImage;
+  final String merchPrice;
+  final String merchName;
+
+  @override
+  State<CustomMerchCard> createState() => _CustomMerchCardState();
+}
+
+class _CustomMerchCardState extends State<CustomMerchCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        height: 290,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.all(5),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    widget.merchImage,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.merchName,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Subtitle',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      '\$  ${widget.merchPrice}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MerchandiseCardSizeColor extends StatefulWidget {
   MerchandiseCardSizeColor({
     required this.gender,
@@ -104,3 +239,135 @@ class _MerchandiseCardSizeColorState extends State<MerchandiseCardSizeColor> {
     );
   }
 }
+
+class MerchGridWithCard extends StatefulWidget {
+  const MerchGridWithCard(
+      {Key? key,
+      required this.name,
+      // required this.size,
+      required this.gender,
+      required this.image,
+      // required this.color,
+      required this.price})
+      : super(key: key);
+
+  final String name;
+  final String image;
+  // final String size;
+  // final Color color;
+  final String gender;
+  final String price;
+
+  @override
+  State<MerchGridWithCard> createState() => _MerchGridWithCardState();
+}
+
+class _MerchGridWithCardState extends State<MerchGridWithCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GridView.builder(
+        shrinkWrap: true,
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        itemCount: 6,
+        itemBuilder: (ctx, i) {
+          return Card(
+            child: Container(
+              height: 290,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(5),
+              child: Stack(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Image.asset(
+                          widget.image,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                      Text(
+                        widget.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '${widget.price}, ${widget.gender}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.0,
+          crossAxisSpacing: 0.0,
+          mainAxisSpacing: 5,
+          mainAxisExtent: 264,
+        ),
+      ),
+    );
+  }
+}
+
+class Merch {
+  late String image;
+  late String name;
+  late String size;
+  late String gender;
+  late Color color;
+  late String price;
+}
+
+void tShirtMen() {
+  Merch tShirt = Merch();
+  tShirt.image = 'images/male_small.png';
+  tShirt.name = 'DiscGo Graphic T';
+  tShirt.size = 'X-Large';
+  tShirt.gender = 'Men';
+  tShirt.color = Colors.grey;
+  tShirt.price = '\$35';
+}
+// class MerchGridList extends StatelessWidget {
+//   const MerchGridList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.count(
+//       // Create a grid with 2 columns. If you change the scrollDirection to
+//       // horizontal, this would produce 2 rows.
+//       crossAxisCount: 2,
+//       // Generate 100 Widgets that display their index in the List
+//       children: List.generate(10, (index) {
+//         return Center(
+//           child: Container(
+//             decoration: BoxDecoration(
+//               border: Border.all(color: Colors.grey, width: 3.0),
+//             ),
+//             padding: const EdgeInsets.all(16.0),
+//             child: Text(
+//               'Item $index',
+//               style: Theme.of(context).textTheme.headlineSmall,
+//             ),
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
--- lib/screens/merchandise.dart
+++ lib/screens/merchandise.dart
@@ -1,141 +1,48 @@
-import 'package:disgo/appBar.dart';
-import 'package:disgo/bottom_nav_back_bar.dart';
-import 'package:disgo/merchandise_card_size_color.dart';
+import 'package:disgo/constants.dart';
 import 'package:flutter/material.dart';
+
+import '../merchandise_card_size_color.dart';
 
 class Merchandise extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavBackBar(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          context: context),
-      body: Container(
-        child: SafeArea(
-          child: Container(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  kAppBar(
-                    onPressedSearch: () {
-                      Navigator.pushNamed(context, '/searchPage');
-                    },
-                    onPressedHome: () {
-                      Navigator.pushNamed(context, '/home');
-                    },
-                    onPressedProfile: () {
-                      Navigator.pushNamed(context, '/profile');
-                    },
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 30,
-                    child: Text('Merchandise'),
-                    color: Colors.grey,
-                    alignment: Alignment.center,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: MerchandiseCardSizeColor(
-                              gender: 'Male',
-                              merchImage: 'images/white_t.png',
-                              size: 'Large',
-                              merchName: 'DiscGo Front Logo T-Shirt',
-                              merchPrice: '19.99',
-                              color: 'White',
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: MerchandiseCardSizeColor(
-                              gender: 'Male',
-                              merchImage: 'images/male_small.png',
-                              size: 'Large',
-                              merchName: 'DiscGo Small Logo T-Shirt',
-                              merchPrice: '19.99',
-                              color: 'Grey',
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: MerchandiseCardSizeColor(
-                              gender: 'Female',
-                              merchImage: 'images/woman_black_t.png',
-                              size: 'Large',
-                              merchName: 'DiscGo Front Logo T-Shirt',
-                              merchPrice: '19.99',
-                              color: 'Black',
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: MerchandiseCardSizeColor(
-                              gender: 'Female',
-                              merchImage: 'images/woman_grey_t.png',
-                              size: 'Large',
-                              merchName: 'DiscGo Front Logo T-Shirt',
-                              merchPrice: '19.99',
-                              color: 'Grey',
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .8,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/checkout');
-                      },
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.shopping_basket,
-                              size: 30,
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Text('Checkout',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .displaySmall
-                                    ?.copyWith(fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+      // bottomNavigationBar: BottomNavBackBar(
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     },
+      //     context: context),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MerchCard(),
+              SizedBox(
+                height: 10,
               ),
-            ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .8,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/checkout');
+                  },
+                  style: kLargeButtonStyle,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.shopping_basket,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text('Checkout', style: kLargeButtonTextStyle),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
